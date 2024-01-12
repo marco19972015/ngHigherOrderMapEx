bab29101121e701440b8f8d68c61b521e394638e
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-12 11:35:31 -0500

    In this version I created an example of using concatMap. First I created the randomDelay method wich returns a number between 500 and 1499 which is later used as milliseconds along with the delay operator. In the ngOnInit I use the range operator to emit values between 1 and 5, then use concatMap and return an observable from the range operator and pipe that down into a dalay operator which takes in milliseonds (this is where we use the randomDelay method.). Lastly I pipe it down once again and use tap to log out the values.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { concatMap, delay, of, range, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'higher-order-mapping-exampels';
+
+  ngOnInit(): void {
+
+    // range emits ints in a given range of values
+    range(1, 5).pipe(
+      // concatMap expected a return of type observable
+      concatMap(i => of(i).pipe(
+        // delay operator takes in the amount of delay in milli seconds
+        delay(this.randomDelay())
+      ))
+    ).pipe(
+      tap((v) => console.log('concatMap:', v))
+    )
+    .subscribe()
+  }
+
+  randomDelay(){
+    // Random delay between 500 and 1499 milliseconds
+    // Math.random() creates a number between 0 and 1
+    // Math.floor lets us round down
+    return Math.floor(Math.random() * 1000) + 500;
+  }
 }
