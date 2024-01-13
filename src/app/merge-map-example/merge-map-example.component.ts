f2cf1fcc9e102108f191966557de6a10d102b97e
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-13 14:08:54 -0500

    In this version I added the same code into merge-map-example.component, the only thing that was changed was the addition of mergeMap to the code. In this case if the code in concat is off, we can see that mergeMap will return number in the range selected whenever they come first, meaning that they don't come in order. Additionally when uncommenting concat code, we can see the the numbers in mergeMap will arrive before the numbers in concat map. This is important to note since the focus on mergeMap is that we have speed over order.

--- src/app/merge-map-example/merge-map-example.component.ts
+++ src/app/merge-map-example/merge-map-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { concatMap, delay, mergeMap, of, range, tap } from 'rxjs';
 
 @Component({
   selector: 'app-merge-map-example',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./merge-map-example.component.css']
 })
 export class MergeMapExampleComponent {
+  // With mergeMap active we will recieve most if not all the values before the values in concat-map
 
+  ngOnInit(): void {
+
+    // range emits ints in a given range of values
+    range(11, 5).pipe(
+      // mergeMap returns the range of numbers as soon as it can
+      mergeMap(i => of(i).pipe(
+        // delay operator takes in the amount of delay in milli seconds
+        delay(this.randomDelay())
+      ))
+    ).pipe(
+      tap((v) => console.log('mergeMap:', v))
+    )
+    .subscribe()
+  }
+
+  randomDelay(){
+    // Random delay between 500 and 1499 milliseconds
+    // Math.random() creates a number between 0 and 1
+    // Math.floor lets us round down
+    return Math.floor(Math.random() * 1000) + 500;
+  }
 }
