1bdae37d5007b8628e1304466e7c4eb714025fa8
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-13 13:53:19 -0500

    Created two components in order to seperate the following examples of the higher order mapping operators examples.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConcatMapExampleComponent } from './concat-map-example/concat-map-example.component';
+import { MergeMapExampleComponent } from './merge-map-example/merge-map-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConcatMapExampleComponent,
+    MergeMapExampleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/merge-map-example/merge-map-example.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merge-map-example',
+  templateUrl: './merge-map-example.component.html',
+  styleUrls: ['./merge-map-example.component.css']
+})
+export class MergeMapExampleComponent {
+
+}
